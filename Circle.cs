38e3027ef64a894b7713c786e9c56494bdd7de58
Yosha Kunnummal <yoshakunnummal@rocketmortgage.com>
2021-10-19 22:59:22 -0400

    First commit of circle time lab

---
+++ Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CircleTime
+{
+    class Circle
+    {
+        public double radius { get; set; }        
+
+        //Got from https://www.c-sharpcorner.com/blogs/count-instances-of-the-class-using-c-sharp
+        public static int count = 0;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            count = count + 1;
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            double circumference =Math.Round(2 * Math.PI * radius,2);
+            return circumference;
+        }
+        public double CalculateArea(double radius)
+        {
+            double area =Math.Round(Math.PI * radius * radius,2);
+            return area;
+        }    
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+//DevBuild Lab : CIRCLE TIME
+//Author: Yosha Kunnummal
+//Date: 10/19/2021
+namespace CircleTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> circleList = new List<string>();
+
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            bool goOn = true;
+            while (goOn)
+            {
+                Console.Write("\nEnter Radius: ");
+                try
+                {
+                    double radius = double.Parse(Console.ReadLine());
+                    Circle c = new Circle(radius);
+                    
+                    double circumference = c.CalculateCircumference(radius);
+                    double area = c.CalculateArea(radius);
+                    Console.WriteLine("Circumference: " + circumference);
+                    Console.WriteLine("Area: " + area);
+                    Console.WriteLine();
+
+                    //Adding the circle radius,area,circumference to a list
+                    circleList.Add(radius.ToString());
+                    circleList.Add(Math.Round(area, 2).ToString());
+                    circleList.Add(Math.Round(circumference, 2).ToString());
+                    
+                    //Printing the list
+                    foreach (string circle in circleList)
+                    {
+                        Console.WriteLine(circle.ToString());                 
+                       
+                    }
+
+                    goOn = Continue();
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Please enter the circle radius.");
+                }
+            }
+        }        
+
+        //A method to continue the application
+        public static bool Continue()
+        {
+            Console.Write("\nContinue? (y/n): ");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer.ToLower() == "y")
+            {
+                return true;
+            }
+            else if (answer.ToLower() == "n")
+            {
+                Console.WriteLine($"Goodbye. You created {CircleTime.Circle.count} Circle object(s).");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("\nThis is not a valid selection. ");
+                Console.WriteLine("Please try again!");
+
+                //This is recursion, calling a method inside itself
+                return Continue();
+            }
+        }
+    }
+}
